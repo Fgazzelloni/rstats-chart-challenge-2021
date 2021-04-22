5d67890b954c4afa5716defb044bc660fd08989d
Federica Gazzelloni <federica@MacBook-Pro-di-iGaz.local>
2021-04-22 23:11:52 +0200

    day21

--- day21_downwards/example_exposure.R
+++ day21_downwards/example_exposure.R
@@ -8,8 +8,8 @@ library(ggpol)
 library(gganimate)
 library(gifski)
 library(extrafont)
-library(showtext)
-showtext_auto(enable=TRUE)
+
+
 
 options(scipen = 999)
 
@@ -19,107 +19,76 @@ df <- read.delim("https://www.prdh.umontreal.ca/BDLC/data/que/Exposures_5x1.txt"
 
 head(df)
 
-df%>%ggplot(aes(x=Year,y=Total,group=Age,color=factor(Age)))+
-  geom_line()
+#df%>%ggplot(aes(x=Year,y=Total,group=Age,color=factor(Age)))+geom_line()
 
-plyr::count(my_df$Age)
+plyr::count(df$Age)
+
+df$Age<-sub("1-4","01-04",df$Age)
+df$Age<-sub("5-9","05-09",df$Age)
+df$Age<-sub("905-099","95+",df$Age)
 
 
 my_df <- df%>%
-  mutate(Age = as.factor(case_when(Age =="1-4" ~ "00-04" ,TRUE ~ Age)))
-
-my_df <- my_df %>%
-  dplyr::mutate(Age = as.factor(as.character(case_when(Age == "5-9" ~ "05-09",TRUE ~ Age))))
-
-#%>%
   arrange(Year,desc(Age))%>%
   filter(!Age=="110+" & !Age=="100-104" & !Age=="105-109" & !Age=="0")%>%
   pivot_longer(cols=c("Female","Male"),names_to="Gender",values_to="Exposure")%>%
   select(-Total)
 
 
-my_df %>%
+my_df_plot <- my_df %>%
   ggplot(aes(x = Age, y = Exposure, fill = Gender)) +
   geom_bar(stat = 'identity') +
-  scale_fill_manual(values = c('#29ffc6', '#d07be0')) +
+  scale_fill_manual(values = c("#d07be0","#29ffc6")) +
   scale_y_continuous(
     breaks = c(0, 10000000, 20000000, 30000000, 40000000),
-    label = c("0M", "10M", "20M","30M", "40M")
-  ) +
-  scale_y_continuous(
-    breaks = c(0, 10000000, 20000000, 30000000, 40000000),
-    label = c("0M", "10M", "20M","30M", "40M")
-  ) +
-  coord_flip(clip = "off") +
-  theme(
-    legend.position = "bottom",
-    plot.background = element_rect(fill = "#5e5556") ,
-    axis.ticks = element_blank(),
-    axis.title.y = element_blank(),
-    legend.title = element_text(family="World of Water"),
-    panel.background = element_blank(),
-    panel.border = element_blank(),
-    strip.background = element_blank(),
-    strip.text.x = element_blank(),
-    panel.grid.minor = element_blank(),
-    panel.grid.major = element_blank(),
-    axis.text = element_text(size = 14),
+    label = c("0M", "10M", "20M","30M", "40M")) +
+    coord_flip(clip = "on") +
+    theme(plot.background = element_rect(fill = "#5e5556") ,
+          panel.background = element_rect(fill = "#5e5556"),
+          panel.border = element_blank(),
+          strip.background = element_blank(),
+          strip.text.x = element_blank(),
+   
+          panel.grid.minor = element_blank(),
+          panel.grid.major = element_blank(),
+    
+    legend.position = "top",
+    legend.title = element_text(family="Luminari",size = 15,face = 'bold',color = 'white'),
     legend.key.size = unit(0.75, 'cm'),
     legend.background = element_blank(),
-    legend.text = element_text(
-      family="World of Water",
-      size = 15,
-      face = 'bold',
-      color = 'white'
-    ),
-    plot.title = element_text(
-      family="World of Water",
-      size = 22,
-      hjust = 0.5,
-      face = 'bold',
-      color = 'white'
-    ),
-    plot.subtitle = element_text(
-      family="World of Water",
-      size = 8,
-      hjust = 0,
-      face = 'bold',
-      color = 'white'
-    ),
-    axis.title.x = element_text(
-      family="World of Water",
-      size = 16,
-      face = 'bold',
-      color = '#242422'
-    ),
-    axis.text.x = element_text(
-      family="World of Water",
-      size = 16,
-      face = 'bold',
-      color = 'white'
-    ),
-    axis.text.y = element_text(
-      family="World of Water",
-      size = 10,
-      face = 'bold',
-      color = 'white'
-    ),
-    plot.caption = element_text(
-      family="World of Water",
-      size = 8,
-      hjust = 1,
-      face = 'bold',
-      color = '#c426c9'
-    ),
+    legend.text = element_text(family="Luminari",size = 15,face = 'bold',color = 'white'),
+    
+    plot.title = element_text(family="Luminari",size = 22,hjust = 0.5,face = 'bold',color = 'white'),
+    plot.subtitle = element_text(family="Luminari",size = 12,hjust = 0,face = 'bold',color = 'white'),
+    plot.caption = element_text(family="Luminari",size = 10,hjust = 1,face = 'bold',color = "white"),
+    
+    axis.line.x = element_line(size=2,color="white"),
+    axis.title.x = element_text(family="Luminari",size = 16,face = 'bold',color = '#242422'),
+    axis.text.x = element_text(family="Luminari",size = 12,face = 'bold', color = 'white'),
+    axis.text.y = element_text(family="Luminari",size = 10,face = 'bold',color = 'white'),
+    axis.ticks = element_blank(),
+    axis.title.y = element_text(family="Luminari",size = 16,face = 'bold',color = '#242422'),
     plot.margin = margin(10,10,10,10)
   )
 
 
-PopPyramid <- my_df +
-  labs(
-    title = 'Quebec Exposure-to-risk Change\n\n{1921-2016}',
-    subtitle = '',
-    y = '\n\nExposure-to-risk',
-    caption = '@fgazzelloni | \n\nData Source: http://www.bdlc.umontreal.ca/CHMD/prov/que/que.htm'
-  )
+exposure_plot <- my_df_plot +
+  labs(title = "Quebec Exposure-to-risk Change\n\n{1921-2016}",
+       subtitle = "Exposure is pivoted downwards by age increasing",
+       y = "Exposure-to-risk (Population in millions)", x="Age",
+       caption = "Viz @fgazzelloni | Data Source: Canadian Human Mortality Database | Downwards Day21") 
+
+
+# saving ######################################
+
+
+ragg::agg_png(here::here("day21_downwards", "downwards_day21.png"),
+              res = 320, width = 14, height = 8, units = "in")
+exposure_plot
+
+dev.off()
+
+
+
+
 
