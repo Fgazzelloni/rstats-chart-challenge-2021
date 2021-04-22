6b5d263f6697be1091d62a39a10b049d86c057f2
Federica Gazzelloni <federica@MacBook-Pro-di-iGaz.local>
2021-04-22 23:31:48 +0200

    resending day 19

--- day19_global_change/global_change_day19.R
+++ day19_global_change/global_change_day19.R
@@ -1,148 +1,125 @@
 
 
-# Global Change Day 19------------------------------------------
+# Global Change Day 19 - Excess of Mortality
 
-# load libraries -------------------------------------
-library(streamgraph)
+# datasource: https://ourworldindata.org/excess-mortality-covid
 
-library(rnaturalearth)
-library(rnaturalearthdata)
-ls("package:rnaturalearthdata")
-library(rgdal)
-library(sp)
 
+
+# load libraries -------------------------
 
 library(tidyverse)
-library(janitor)
-library(scales)
-library(magick)
-library(here)
-library(sf)
+library(streamgraph)
+library(lubridate)
 
 
-# load data --------------------------------------
-library(downloader)
-url="https://ihmecovid19storage.blob.core.windows.net/latest/ihme-covid19.zip"
-download(url, dest="ihme-covid19_latest.zip", mode="wb") 
-unzip("ihme-covid19_latest.zip")
+# load data and manipulation ------------------------------------
+excess_of_mortality<-read.csv("https://raw.githubusercontent.com/owid/covid-19-data/master/public/data/excess_mortality/excess_mortality.csv")
+head(excess_of_mortality)
+names(head(excess_of_mortality))
 
-latest_IHME <- read.csv("reference_hospitalization_all_locs.csv")
-deaths_IHME <- latest_IHME%>%select(date,location_name,contains("dea"),-deaths_data_type)
-names(deaths_IHME)
+excess_of_mortality[is.na(excess_of_mortality)] <- 0
 
-deaths_long <- deaths_IHME%>%
-  mutate(name=location_name)%>%
-  select(-location_name)%>%
-  pivot_longer(cols=contains("dea"),
-               names_to="deaths",
-               values_to="value")
+avg_std_excess <- excess_of_mortality%>%group_by(location)%>%summarize(avg = mean(average_deaths_2015_2019_all_ages))
 
-deaths_long$value[is.na(deaths_long$value)] <- 0
-sum(is.na(deaths_long))
-utils::write.csv(deaths_long,"deaths_IHME.csv")
+mean(avg_std_excess$avg) # 8247.341
+
+plyr::count(excess_of_mortality$location)
 
 
-###################################
-
-df <-read.csv("deaths_IHME.csv")
-head(df)
-
+df_plot <-excess_of_mortality%>%
+  group_by(date, location) %>%
+  tally(wt=p_scores_all_ages) 
 
 
-plyr::count(df$deaths)
-
-only_deaths <- df%>%filter(deaths==c("deaths_lower","deaths_mean","deaths_upper"))
-
-# use rnaturalearthdata -------------------------------------
-
-# ?ne_countries() - Get natural earth world country polygons
-world_raw <- ne_countries(scale = 50, returnclass = "sf") %>% 
-  clean_names()
-
-names(world_raw)
-glimpse(world_raw)
-
-# select desired variables
-w_df <-world_raw%>%
-  select(name,geometry,pop_est,gdp_md_est,gdp_year)
-
-# set NAs as 0 values
-w_df$gdp_year[is.na(w_df$gdp_year)] <- 0
-
-glimpse(w_df)
-
-# CRS coordinate ref systems:
-# https://www.nceas.ucsb.edu/sites/default/files/2020-04/OverviewCoordinateReferenceSystems.pdf
-
-df_join <- df %>% 
-  inner_join(w_df, by = c("name")) %>%
-  st_as_sf(crs = 4326) %>% 
-  st_transform(
-    "+proj=vandg +lon_0=0 +x_0=0 +y_0=0 +R_A +datum=WGS84 +units=m +no_defs"
-  )
-
-head(df)
-library(forcats)
-
-top_15 <- df_join %>% 
-  st_centroid() %>% 
-  cbind(st_coordinates(.)) %>% 
-  st_drop_geometry() %>% 
-  mutate(name = fct_lump(name, 15, w = pop_est)) %>% 
-  filter(as.character(entity) != "Other")
+# function "sg_add_marker()" found here: https://github.com/hrbrmstr/streamgraph/blob/master/R/marker.r
+sg_add_marker <- function(sg, x, label="", stroke_width=0.5, stroke="#7f7f7f", space=5,
+                          y=0, color="#7f7f7f", size=12, anchor="start") {
+  
+  if (inherits(x, "Date")) { x <- format(x, "%Y-%m-%d") }
+  
+  mark <- data.frame(x=x, y=y, label=label, color=color, stroke_width=stroke_width, stroke=stroke,
+                     space=space, size=size, anchor=anchor, stringsAsFactors=FALSE)
+  
+  if (is.null(sg$x$markers)) {
+    sg$x$markers <- mark
+  } else {
+    sg$x$markers <- bind_rows(mark, sg$x$markers)
+  }
+  
+  sg
+  
+}
 
 
+top_10<-df_plot%>%arrange(-n)%>%group_by(location)%>%summarize(sum=sum(n))%>%ungroup()%>%arrange(-sum)
+plyr::count(df_plot$location)
+  
+
+# plotting -----------------------------------------------
+streamgraph(data= df_plot, "location", "n", "date", offset="zero", 
+                    interpolate="cardinal",interactive=FALSE) %>%
+  sg_axis_y(10) %>%
+  # vertical line and label
+  sg_add_marker(x=as.Date("2020-01-05"),  y = 408, label= "",
+                stroke_width = 0.8,
+                stroke = "#7f7f7f", space = 5, color = "#7f7f7f",
+                size = 12, anchor = "start") %>% 
+  sg_add_marker(x=as.Date("2020-04-08"), label= "",
+                stroke_width = 0.8,
+                stroke = "#7f7f7f", space = 5, y = 0, color = "#7f7f7f",
+                size = 12, anchor = "end") %>% 
+  sg_add_marker(x=as.Date("2020-11-01"), label= "",
+                stroke_width = 0.8,
+                stroke = "#7f7f7f", space = 5, y = 0, color = "#7f7f7f",
+                size = 12, anchor = "start") %>% 
+  sg_add_marker(x=as.Date("2021-01-01"), label= "",
+                stroke_width = 0.8,
+                stroke = "#7f7f7f", space = 5, y = 0, color = "#7f7f7f",
+                size = 12, anchor = "start") %>% 
+  sg_add_marker(x=as.Date("2021-03-01"), label= "",
+                stroke_width = 0.8,
+                stroke = "#7f7f7f", space = 5, y = 0, color = "#7f7f7f",
+                size = 12, anchor = "start") %>% 
+  # annotate the top ten countries
+  sg_annotate(label="Top Ten countries with the highest P-score",size=10,x=as.Date("2020-01-7"), y=1.30*10^3, color="black") %>%
+  sg_annotate(label="1 - Equador",size=10,x=as.Date("2020-01-7"), y=1.25*10^3, color="black") %>%
+  sg_annotate(label="2 - Mexico",size=10,x=as.Date("2020-01-7"), y=1.20*10^3, color="black") %>%
+  sg_annotate(label="3 - Colombia",size=10,x=as.Date("2020-01-7"), y=1.15*10^3, color="black") %>%
+  sg_annotate(label="4 - Poland",size=10,x=as.Date("2020-01-7"), y=1.10*10^3, color="black") %>%
+  sg_annotate(label="5 - Chile",size=10,x=as.Date("2020-01-7"), y=1.05*10^3, color="black") %>%
+  sg_annotate(label="6 - Czechia",size=10,x=as.Date("2020-01-7"), y=1.0*10^3, color="black") %>%
+  sg_annotate(label="7 - United States",size=10,x=as.Date("2020-01-7"), y=0.95*10^3, color="black") %>%
+  sg_annotate(label="8 - Slovakia",size=10,x=as.Date("2020-01-7"), y=0.90*10^3, color="black") %>%
+  sg_annotate(label="9 - Slovenia",size=10,x=as.Date("2020-01-7"), y=0.85*10^3, color="black") %>%
+  sg_annotate(label="10 - Spain",size=10,x=as.Date("2020-01-7"), y=0.80*10^3, color="black") %>%
+  # set a title and a subtile
+  sg_annotate(label="How have excess mortality changed Globally due to Covid19?", 
+              size=30,x=as.Date("2020-01-23"), y=1.57*10^3, color="blue") %>%
+  sg_annotate(label ="The P-score is the percentage of number of deaths in a given period obtained as proportion of the deaths difference with a reference period",
+              size=10, x=as.Date("2020-01-07"), y=1.45*10^3, color="black") %>%
+  # annotate formulation for calculating the P-score
+  sg_annotate(label = "Excess of Deaths_w2020 = Deaths_w2020 - Avg_Deaths_w2015-2019",
+              size=10, x=as.Date("2020-05-13"), y=1.10*10^3, color="black") %>%
+  sg_annotate(label = "P-score = (Deaths_2020-2021 - Avg_Deaths_2015-2019)/ Avg_Deaths_2015-2019",
+              size=10, x=as.Date("2020-05-13"), y=1.05*10^3, color="black") %>%
+  # set a caption
+  sg_annotate(label = "Viz Federica Gazzelloni | Data: OurWorldinData | Global Change Day19",
+              size=10, x=as.Date("2020-06-23"), y=-0.1*10^3, color="blue") %>%
+  # full color
+  sg_fill_tableau() 
   
 
 
-# plotting -------------------------
+
+# saving ######################################
+library(webshot)
+
+# Make a webshot in png : Low quality - but you can choose shape
+webshot(here::here("day19_global_change", "global_change_day19.png"), 
+        delay = 0.2 , cliprect = c(440, 0, 1000, 10))
 
 
-ggplot(data= deaths_long, 
-       aes(x=date, y=value)) + 
-  geom_point()  
-
-
-plyr::count(only_deaths$name)
-my_df <-subset(only_deaths,name==c("China","United States of America","Brazil"))
-
-
-
-range(my_df$value)
-
-
-my_df$date<-as.Date(my_df$date)
-class(my_df$date)
-library(lubridate)
-
-plot_df<-my_df%>%mutate(month=month(date),
-               year=year(date),
-               month.year=paste(month,year,sep="-"))%>%
-  arrange(month.year)%>%
-  group_by(month.year,date,name,deaths)%>%
-  summarize(avg_value=mean(value))%>%
-              ungroup()
-
-glimpse(plot_df)
-
-plot_df$month.year<-as.factor(as.Date(plot_df$month.year,"%m%Y"))
-
-plot_df_up <-plot_df%>%filter(deaths=="deaths_upper")
-
-library(xkcd)
-xrange <-range(plot_df_up$month.year)
-yrange <- range(plot_df_up$avg_value)
-ggplot(plot_df_up) +
-  geom_point(aes(x=factor(month.year),y=avg_value))+
-xkcdaxis(xrange,yrange)
-
-streamgraph(data = plot_df, 
-            key="name", 
-            value="avg_value", 
-            date="month.year", 
-            height="300px", 
-            width="1000px") %>%
-  sg_legend(show=TRUE, label="deaths: ")
 
 
 
